4bf6046257a8c05a49e528a0013354742c5f05d9
Koptelov Nikita <nkoptelov@ozon.ru>
2018-07-10 20:44:36 +0300

    comments

--- main.go
+++ main.go
@@ -13,12 +13,19 @@ type chiWrapper struct {
 	r chi.Router
 }
 
+// HandleFunc is practically the same, but instead of alias http.HandlerFunc it accepts
+// its expanded version:
+// type HandlerFunc func(ResponseWriter, *Request)
 func (w chiWrapper) HandleFunc(p string, f func(http.ResponseWriter, *http.Request)) {
+	// func(ResponseWriter,*Request) == http.HandlerFunc, so no conversion needed
 	w.r.HandleFunc(p, f)
 }
 
 func main() {
+	// std mux implements the interface
 	var _ handler = http.NewServeMux()
+	// wrapper implements the interface
 	var _ handler = chiWrapper{r: chi.NewMux()}
+	// chi does not
 	var _ handler = chi.NewMux()
 }
